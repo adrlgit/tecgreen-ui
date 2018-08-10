b74d190effd7b2c0f0484b3af8882738c09a28f4
André luis <luis.alms91@gmail.com>
2018-08-10 01:37:05 -0300

    instalação das bibliotecas css primeng e criação da listagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { TableModule } from 'primeng/table';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ListagemComponent } from './listagem/listagem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ListagemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/listagem/listagem.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listagem',
+  templateUrl: './listagem.component.html',
+  styleUrls: ['./listagem.component.css']
+})
+export class ListagemComponent implements OnInit {
+
+vendas: Array<any>;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
