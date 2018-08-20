a995585907c84bfbc7b631eecc929c5de35eafd0
André luis <luis.alms91@gmail.com>
2018-08-20 08:40:55 -0300

    Atualização do layoute

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import localePt from '@angular/common/locales/pt';
-import { registerLocaleData } from '@angular/common';
 
-
-import { FormsModule } from '@angular/forms';
-import { CurrencyMaskModule } from 'ng2-currency-mask';
-import { TableModule } from 'primeng/table';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputTextModule } from 'primeng/inputtext';
-import { PanelModule } from 'primeng/panel';
-import { ButtonModule } from 'primeng/button';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ListagemComponent } from './listagem/listagem.component';
-import { VendaCadastroComponent } from './venda-cadastro/venda-cadastro.component';
-import { HomeComponent } from './home/home.component';
-import { UsuarioComponent } from './usuario/usuario.component';
-import { RecepcaoComponent } from './recepcao/recepcao.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    ListagemComponent,
-    VendaCadastroComponent,
-    HomeComponent,
-    UsuarioComponent,
-    RecepcaoComponent
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+  
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-
-    CurrencyMaskModule,
-    TableModule,
-    DropdownModule,
-    InputTextModule,
-    PanelModule,
-    ButtonModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
